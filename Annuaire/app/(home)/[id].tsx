0bfaf7b3beef2e51ec16a431e9ad1e42b23997ed
training-dev <contact@training-dev.fr>
2026-03-12 12:26:22 +0100

    filter contact

--- Annuaire/app/(home)/[id].tsx
+++ Annuaire/app/(home)/[id].tsx
@@ -1,11 +1,13 @@
+import { createAsyncStorage } from '@react-native-async-storage/async-storage';
 import { useLocalSearchParams } from "expo-router";
-import { UserPlus } from "lucide-react-native";
+import { UserMinus, UserPlus } from "lucide-react-native";
 import { useEffect, useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Detail({ }) {
     const { id } = useLocalSearchParams();
     const [user, setUser] = useState<any>({});
+    const [isContact, setIsContact] = useState(false);
 
     useEffect(() => {
         async function loadUser() {
@@ -14,25 +16,73 @@ export default function Detail({ }) {
             setUser(data);
         }
         loadUser();
-    }, []);
+    }, [id]);
+
+    useEffect(() => {
+        async function checkContact() {
+            const storage = createAsyncStorage("contacts");
+            const result = await storage.getItem('contacts');
+            const contactList = result ? JSON.parse(result) : [];
+            console.log(contactList)
+            for (let contact of contactList) {
+                if (contact.id === user.id) {
+                    setIsContact(false);
+                    return;
+                }
+                setIsContact(true)
+            }
+        }
+        checkContact();
+    },[id])
+
+    const handleToggleContact = async () => {
+        const storage = createAsyncStorage("contacts");
+        const result = await storage.getItem('contacts');
+        const contactList = result ? JSON.parse(result) : [];
+        if (isContact) {
+            let newContactList = [];
+            for (let contact of contactList) {
+                if (contact.id !== user.id) {
+                    newContactList.push(contact);
+                }
+            }
+            await storage.setItem('contacts', JSON.stringify(newContactList));
+            setIsContact(false);
+        } else {
+            contactList.push(user);
+            await storage.setItem('contacts', JSON.stringify(contactList));
+            setIsContact(true);
+        }
+
+    }
+
     return (
         <View>
             <View style={styles.imageContainer}>
                 <Image style={styles.image} source={{ uri: user.image }} />
                 <Text style={styles.name}>{user.firstName} {user.lastName}</Text>
             </View>
-            <Pressable style={styles.addContact}>
-                   <UserPlus /> Ajouter aux contacts
+            <Pressable style={isContact?styles.removeContact:styles.addContact} onPress={handleToggleContact}>
+                {!isContact &&
+                    <Text style={styles.addContactText}>
+                        <UserPlus /> Ajouter aux contacts
+                    </Text>
+                }
+                {isContact &&
+                    <Text style={styles.addContactText}>
+                        <UserMinus /> Retirer des contacts
+                    </Text>
+                }
             </Pressable>
             <View style={styles.data}>
                 <Text style={styles.label}>
-                    Email : 
+                    Email :
                 </Text>
                 <Text style={styles.value}>
                     {user.email}
                 </Text>
                 <Text style={styles.label}>
-                    Université : 
+                    Université :
                 </Text>
                 <Text style={styles.value}>
                     {user.university}
@@ -55,7 +105,7 @@ const styles = StyleSheet.create({
         aspectRatio: 1,
         objectFit: "contain"
     },
-    data:{
+    data: {
         padding: 15
     },
     name: {
@@ -67,17 +117,31 @@ const styles = StyleSheet.create({
         fontSize: 18
     },
     value: {
-        
+
     },
-    addContact:{
+    addContact: {
         backgroundColor: "#1565c0",
-        color: "white",
+
         padding: 8,
         margin: 15,
-        alignItems: "center",
         flexDirection: "row",
-        justifyContent:"center",
+        justifyContent: "center",
         gap: 10,
         borderRadius: 10
+    },
+    removeContact: {
+        backgroundColor: "red",
+
+        padding: 8,
+        margin: 15,
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: 10,
+        borderRadius: 10
+    },
+    addContactText: {
+        color: "white",
+        alignItems: "center",
+        justifyContent: "center",
     }
 })
--- Annuaire/app/(home)/contact.tsx
+++ Annuaire/app/(home)/contact.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
+import UserList from '@/components/UserList/UserList';
+import { createAsyncStorage } from '@react-native-async-storage/async-storage';
+import { useIsFocused } from '@react-navigation/native';
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 
-export default function Contact(){
+export default function Contact() {
+    const isFocused = useIsFocused();
+    const [userList, setUserList] = useState([]);
+    useEffect(() => {
+        async function checkContact() {
+            const storage = createAsyncStorage("contacts");
+            const result = await storage.getItem('contacts');
+            const contactList = result ? JSON.parse(result) : [];
+            setUserList(contactList);
+        }
+        if (isFocused) {
+            checkContact();
+        }
+    }, [isFocused])
+
+    const search = async (term:string) => {
+        const storage = createAsyncStorage("contacts");
+        const result = await storage.getItem('contacts');
+        const contactList = result ? JSON.parse(result) : [];
+        setUserList(contactList.filter((user:{firstName:string,lastName:string}) => user.firstName.includes(term) || user.lastName.includes(term)))
+    }
     return (
         <View>
-            <Text>contact</Text>
+            <UserList userList={userList} onSearch={(term: string) => search(term)} />
         </View>
     )
 }
